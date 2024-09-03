e8fceec44e7523c7b5fa47b840581ed449f5551a
Leo Singer <leo.singer@ligo.org>
2024-09-03 07:46:10 -0400

    Run pre-commit hooks for the first time

--- conesearch_alchemy/math.py
+++ conesearch_alchemy/math.py
@@ -1,9 +1,10 @@
 """Define SQLAlchemy bindings for trigonometric functions."""
+
+from sqlalchemy.ext.compiler import compiles
 from sqlalchemy.sql.functions import GenericFunction, ReturnTypeFromArgs
 from sqlalchemy.sql.sqltypes import Float
-from sqlalchemy.ext.compiler import compiles
 
-__all__ = ['pi']
+__all__ = ["pi"]
 mod_dict = globals()
 
 
@@ -13,8 +14,9 @@ class pi(GenericFunction):
 
 def _compile_trigd_default(trig):
     def compilefunc(element, compiler, **kw):
-        arg, = element.clauses
+        (arg,) = element.clauses
         return compiler.process(trig(arg * pi() / 180.0), **kw)
+
     return compilefunc
 
 
@@ -22,14 +24,19 @@ def _compile_trigd_postgresql(element, compiler, **kw):
     return compiler.visit_function(element)
 
 
-for name in ['cos', 'sin', 'tan']:
-    mod_dict[name] = type(name, (ReturnTypeFromArgs,), {'inherit_cache': True})
+for name in ["cos", "sin", "tan"]:
+    mod_dict[name] = type(name, (ReturnTypeFromArgs,), {"inherit_cache": True})
     __all__.append(name)
-    named = f'{name}d'
-    mod_dict[named] = type(named, (ReturnTypeFromArgs,), {'inherit_cache': True})
+    named = f"{name}d"
+    mod_dict[named] = type(named, (ReturnTypeFromArgs,), {"inherit_cache": True})
     compiles(mod_dict[named])(_compile_trigd_default(mod_dict[name]))
-    compiles(mod_dict[named], 'postgresql')(_compile_trigd_postgresql)
+    compiles(mod_dict[named], "postgresql")(_compile_trigd_postgresql)
 
-del mod_dict, GenericFunction, ReturnTypeFromArgs, \
-    _compile_trigd_default, _compile_trigd_postgresql
+del (
+    mod_dict,
+    GenericFunction,
+    ReturnTypeFromArgs,
+    _compile_trigd_default,
+    _compile_trigd_postgresql,
+)
 __all__ = tuple(__all__)
--- conesearch_alchemy/point.py
+++ conesearch_alchemy/point.py
@@ -1,14 +1,15 @@
 """Spatial indexing for astronomical point coordinates."""
+
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.ext.hybrid import hybrid_method, hybrid_property
 from sqlalchemy.schema import Column, Index
 from sqlalchemy.sql import and_
 from sqlalchemy.types import Float
 
-from .math import sind, cosd
+from .math import cosd, sind
 from .util import InheritTableArgs
 
-__all__ = ('Point',)
+__all__ = ("Point",)
 
 
 class Point(InheritTableArgs):
@@ -32,9 +33,11 @@ class Point(InheritTableArgs):
             A tuple of the x, y, and z coordinates.
 
         """
-        return (cosd(self.ra) * cosd(self.dec),
-                sind(self.ra) * cosd(self.dec),
-                sind(self.dec))
+        return (
+            cosd(self.ra) * cosd(self.dec),
+            sind(self.ra) * cosd(self.dec),
+            sind(self.dec),
+        )
 
     @hybrid_method
     def within(self, other, radius):
@@ -57,13 +60,15 @@ class Point(InheritTableArgs):
         carts = (obj.cartesian for obj in (self, other))
         # Evaluate boolean expressions for bounding box test
         # and dot product
-        terms = ((lhs.between(rhs - 2 * sin_radius, rhs + 2 * sin_radius),
-                  lhs * rhs) for lhs, rhs in zip(*carts))
+        terms = (
+            (lhs.between(rhs - 2 * sin_radius, rhs + 2 * sin_radius), lhs * rhs)
+            for lhs, rhs in zip(*carts)
+        )
         bounding_box_terms, dot_product_terms = zip(*terms)
         return and_(*bounding_box_terms, sum(dot_product_terms) >= cos_radius)
 
     @declared_attr
     def __table_args__(cls):
         *args, kwargs = super().__table_args__
-        index = Index(f'ix_{cls.__tablename__}_point', *cls.cartesian)
+        index = Index(f"ix_{cls.__tablename__}_point", *cls.cartesian)
         return (*args, index, kwargs)
